a55973b3d939bc2ab94669bdd123a8f4b8c5c06f
Your Name <joe.dem@icloud.com>
2024-09-29 22:37:31 +0200

    GUI

---
+++ Files/pdfmerge.py
@@ -0,0 +1,9 @@
+import customtkinter
+from tkinter import messagebox
+from pytube import YouTube
+
+
+
+class PDFMerge:
+    def __init__(self, app):
+        self.app = app
---
+++ main.py
@@ -0,0 +1,23 @@
+from PyPDF2 import PdfMerger
+import tkinter as tk
+from pytube import YouTube
+from tkinter import messagebox
+import customtkinter
+from Files import pdfmerge
+from Files.pdfmerge import PDFMerge
+
+customtkinter.set_appearance_mode("system")
+customtkinter.set_default_color_theme("blue")
+
+
+app = customtkinter.CTk()
+app.geometry("960x540")
+app.title("PDF Tool")
+
+
+
+
+
+
+
+app.mainloop()
