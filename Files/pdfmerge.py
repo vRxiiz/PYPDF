3584504005e47f1eca9782d2046c455c1bc16913
Joel Demir <157913684+vRxiiz@users.noreply.github.com>
2024-10-01 16:41:44 +0200

    test

--- Files/pdfmerge.py
+++ Files/pdfmerge.py
@@ -1,9 +1,16 @@
 import customtkinter
 from tkinter import messagebox
-from pytube import YouTube
+#test
+
 
 
 
 class PDFMerge:
     def __init__(self, app):
         self.app = app
+
+    def pdfmerge_function(self):
+        title = customtkinter.CTkLabel(self.app, text="Insert your PDF files", font=("arial", 20))
+        title.pack(padx=20, pady=20)
+        entry = customtkinter.CTkEntry(self.app, width=400, placeholder_text="YouTube Link hier einfügen")
+        entry.pack(padx=20, pady=20)
--- main.py
+++ main.py
@@ -1,23 +1,38 @@
 from PyPDF2 import PdfMerger
 import tkinter as tk
-from pytube import YouTube
 from tkinter import messagebox
 import customtkinter
-from Files import pdfmerge
 from Files.pdfmerge import PDFMerge
 
 customtkinter.set_appearance_mode("system")
 customtkinter.set_default_color_theme("blue")
 
-
 app = customtkinter.CTk()
 app.geometry("960x540")
 app.title("PDF Tool")
 
+pdfmerge_instance = PDFMerge(app)
+
+def reset_gui():
+    for widget in app.winfo_children():
+        widget.destroy()
+    initialize_gui()
+
+    reset_button = customtkinter.CTkButton(app, text="Hauptmenü", command=reset_gui)
+    reset_button.place(x=50, y=50)
 
 
 
+def pdfmerge_import():
+    pdfmerge_instance.pdfmerge_function()
+    button.destroy()
 
+def initialize_gui():
+    global button
+    button = customtkinter.CTkButton(app, text="PDF Merge ", command=pdfmerge_import(), height=100,
+        width=200,)
+    button.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
 
-
+if __name__ == '__main__':
+    initialize_gui()
 app.mainloop()
